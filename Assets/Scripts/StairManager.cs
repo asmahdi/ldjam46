da268deb808818960efcac5ba51a3d70855b36b8
Abu Sabiq Mahdi <nipun.mahdi@gmail.com>
2020-04-19 19:42:49 +0600

    Initial stage
    Created base visuals

---
+++ Assets/Scripts/StairManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class StairManager : MonoBehaviour
+{
+    [Range(0,6)]
+    public float range;
+
+    public bool invert;
+    public GameObject[] stairObj;
+    public float height;
+
+    private int i;
+
+   
+
+    private void Update()
+    {
+        height = transform.position.y;
+
+        if (invert)
+        {
+            for (i = stairObj.Length-1; i >= 0; i--)
+            {
+                stairObj[i].transform.position = new Vector3(stairObj[i].transform.position.x,
+                                                                    height + range * (5-i) / 6,
+                                                                  stairObj[i].transform.position.z);
+            }
+        }
+        else
+        {
+            for (i = 0; i < stairObj.Length; i++)
+            {
+                stairObj[i].transform.position = new Vector3(stairObj[i].transform.position.x,
+                                                                    height + range * i / 6,
+                                                                  stairObj[i].transform.position.z);
+            }
+        }
+       
+        
+
+    }
+}
---
+++ Assets/Scripts/StairTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class StairTrigger : MonoBehaviour
+{
+
+    public Animator stair;
+    public StairManager stairManager;
+    public bool invert;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (invert)
+        {
+            stairManager.invert = true;
+        }
+        else
+        {
+            stairManager.invert = false;
+        }
+
+       if ( other.tag == "Player")
+        {
+            stair.SetBool("stair_up",true);
+        }
+    }
+
+
+}
