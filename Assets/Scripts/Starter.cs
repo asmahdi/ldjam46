ba8e4ee1054459fd20cbef8cf5a64973b5114fa8
Abu Sabiq Mahdi <nipun.mahdi@gmail.com>
2020-04-21 01:24:09 +0600

    Fixed icon and player settings

---
+++ Assets/Scripts/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        Initiate.Fade("Art", Color.black, 1);
+    }
+}
